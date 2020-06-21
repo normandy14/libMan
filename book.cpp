4543e13ae49fabae8569dfb033261d5797f7ca38
normandy14 <normandy14@protonmail.com>
2020-06-21 09:52:46 -0400

    Add library class, book struct. Implement addbook method

---
+++ book.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct Book {
+  int id;
+  string name;
+  string author;
+  string category;
+  int quantity;
+  int price;
+  int rackNo;
+};
---
+++ library.cpp
@@ -0,0 +1,55 @@
+#include <string>
+// #include "hashmap.cpp"
+#include "book.cpp"
+
+using namespace std;
+
+class Library {
+  //private:
+    //Hashmap books;
+  public:
+    void addbook(int idIn, string nameIn, string authorIn, string categoryIn, int quantityIn, int priceIn, int rackNoIn);
+    void deletebook(int id);
+    void searchbook(int id);
+    void issuebook(int id);
+    void viewbook(int id);
+    void printbook(Book book);
+};
+
+void Library::addbook(int idIn, string nameIn, string authorIn, string categoryIn, int quantityIn, int priceIn, int rackNoIn) {
+  Book book;
+  book.id = idIn;
+  book.name = nameIn;
+  book.author = authorIn;
+  book.category = categoryIn;
+  book.quantity = quantityIn;
+  book.price = priceIn;
+  book.rackNo = rackNoIn;
+  printbook(book);
+}
+
+void Library::deletebook(int id) {
+  
+}
+
+void Library::searchbook(int id) {
+  
+}
+
+void Library::issuebook(int id) {
+  
+}
+
+void Library::viewbook(int id) {
+  
+}
+
+void Library::printbook(Book book) {
+  cout << "id: " << book.id << endl;
+  cout << "name: " << book.name << endl;
+  cout << "author: " << book.author << endl;
+  cout << "category: " << book.category << endl;
+  cout << "quantity: " << book.quantity << endl;
+  cout << "price: " << book.price << endl;
+  cout << "rackNo: " << book.rackNo << endl;
+}
